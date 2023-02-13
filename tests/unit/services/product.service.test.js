216152d5c0c6f6363539ab644165911703155dc4
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-02-13 19:30:14 -0300

    req 5 - product service

--- tests/unit/services/product.service.test.js
+++ tests/unit/services/product.service.test.js
@@ -5,23 +5,45 @@ const { allProducts } = require('./product.service.mock');
 const { productModel } = require('../../../src/models');
 const { productService } = require('../../../src/services');
 
-describe('Verificando service de products', function () {
-  it('retorna a lista completa de todos os produtos', async function () {
-    sinon.stub(productModel, 'findAll').resolves(allProducts);
-    
-    const result = await productService.findAll();
+describe('Verificando camada service de products', function () {
+  describe('Testando o GET', function () {
+    it('retorna a lista completa de todos os produtos', async function () {
+      sinon.stub(productModel, 'findAll').resolves(allProducts);
+      
+      const result = await productService.findAll();
+  
+      expect(result.type).to.be.equal(null);
+      expect(result.message).to.deep.equal(allProducts);
+    });
+  
+    it('retorna a lista completa de todos os produtos', async function () {
+      sinon.stub(productModel, 'findById').resolves([[allProducts[1]]]);
+      
+      const result = await productService.findById(2);
+  
+      expect(result.type).to.be.equal(null);
+      expect(result.message).to.deep.equal([[allProducts[1]]]);
+    });
 
-    expect(result.type).to.be.equal(null);
-    expect(result.message).to.deep.equal(allProducts);
+    it('Retorna um erro caso o produto não seja encontrado', async function () {
+      sinon.stub(productModel, 'findById').resolves({ message: 'Product not found' });
+      
+      const result = await productService.findById(100);
+  
+      expect(result.type).to.be.equal(404);
+      expect(result.message).to.deep.equal({ message: 'Product not found' });
+    });
   });
 
-  it('retorna a lista completa de todos os produtos', async function () {
-    sinon.stub(productModel, 'findById').resolves([[allProducts[1]]]);
-    
-    const result = await productService.findById(2);
+  describe('Testando o POST', function () {
+    it('Insere um novo produto com sucesso', async function () {
+      sinon.stub(productModel, 'insert').resolves({ id: 1, name: 'Anaklusmus'})
 
-    expect(result.type).to.be.equal(null);
-    expect(result.message).to.deep.equal([[allProducts[1]]]);
+      const result = await productService.newProduct(2);
+
+      expect(result.type).to.be.equal(null);
+      expect(result.message).to.deep.equal({ id: 1, name: 'Anaklusmus'});
+    })
   });
   
    afterEach(function () {
