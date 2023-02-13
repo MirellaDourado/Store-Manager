1d7b06f0d7b0e5760adceaed53c2498bdd002645
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-02-13 20:11:57 -0300

    testanado correção de erro

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,7 +2,6 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
 });
